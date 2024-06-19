48c31735542c0b11454f13f455a90b7c92e3b486
AlejoNardon <alejonardon2002@gmail.com>
2024-06-19 12:03:08 -0300

    modales como funcion

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+// modal de contacto
+let contactModal = ` 
+<div class="modalContact">
+    <h2 class="titContact">Contacto</h2>
+    <p class="textContact">Si tienes alguna duda o sugerencia, no dudes en contactarnos.</p>
+</div>`;
+displayModal(contactModal);
