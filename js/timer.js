5ef3a13f54b0bdf330e93d0806f2c51060e246db
AlejoNardon <alejonardon2002@gmail.com>
2024-06-21 20:23:30 -0300

    agg sonidos al juego y logica al terminar el tiempo

--- js/timer.js
+++ js/timer.js
@@ -1,15 +1,35 @@
-localStorage.setItem(
-	"configPlay",
-	JSON.stringify({ time: 3, name: "Jugador 1" })
-);
+function finishTime() {
+	document.getElementById("buttonAccept").disabled = true;
+	let ring = new Audio("../sounds/ring.mp3");
+	ring.play();
+	displayGameOverModal();
+	// ocultar el inicio del juego
+	document.getElementById("startGame").style.display = "";
+	// mostrar el juego
+	document.getElementById("viewGame").style.display = "none";
+	let rank = JSON.parse(localStorage.getItem("ranking"));
+	let configPlay = JSON.parse(localStorage.getItem("configPlay"));
+	let rankPlay = {
+		name: configPlay.name,
+		points: localStorage.getItem("points"),
+		date: configPlay.day,
+		time: configPlay.time,
+	};
+
+	if (rank === null || rank === undefined || rank === "" || rank.length === 0) {
+		localStorage.setItem("ranking", JSON.stringify([rankPlay]));
+        } else {
+        rank.push(rankPlay)
+        localStorage.setItem("ranking", JSON.stringify(rank));
+	}
+	createTableRanking();
+}
+
 function startTimer() {
-	let buttonAccept = document.getElementById("buttonAccept");
 	let timerDisplay = document.getElementById("timer");
 	let time = JSON.parse(localStorage.getItem("configPlay")).time;
 	let totalTime = time * 60; // 3 minutos en segundos
-	
-
-
+	// let totalTime = 10; // 3 minutos en segundos
 
 	let interval = setInterval(function () {
 		let minutes = Math.floor(totalTime / 60);
@@ -20,14 +40,15 @@ function startTimer() {
 		seconds = seconds < 10 ? "0" + seconds : seconds;
 
 		timerDisplay.textContent = minutes + ":" + seconds;
-		if (totalTime <= 10) {
+		if (totalTime === 10) {
 			timerDisplay.classList.add("timerRed");
+			let clock = new Audio("../sounds/clock.mp3");
+			clock.play();
 		}
 
 		if (totalTime <= 0) {
 			clearInterval(interval);
-			buttonAccept.disabled = true;
-            modalFinishGame();
+			finishTime();
 		} else {
 			totalTime--;
 		}
